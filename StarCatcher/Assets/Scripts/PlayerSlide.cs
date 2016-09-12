6c8d105d6596ed9fd6a1281713806d57c3e31fae
Unknown <dalinara13@gmail.com>
2016-09-12 13:47:22 -0700

    First  Unity commit.

---
+++ StarCatcher/Assets/Scripts/PlayerSlide.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSlide : MonoBehaviour 
+{
+	private CharacterController myCC;
+	public int slideDuration = 10;
+	public float slideTime = 0.01f;
+
+	IEnumerator Slide()
+	{
+		int i = 0;
+		//while iterations
+		//wait for seconds
+		//move the character controller
+		while(i < slideDuration)
+		{
+			i++;
+			yield return new WaitForSeconds (slideTime);
+			print ("Sliding");
+			//Move the character controller
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		myCC = GetComponent<CharacterController> ();
+		StartCoroutine(Slide());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
