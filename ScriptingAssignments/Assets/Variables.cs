5fb26ab6d5c9f41cd0652b94942f3ff173077f30
Unknown <dalinara13@gmail.com>
2016-09-15 23:35:28 -0700

    Variables Assignment

---
+++ ScriptingAssignments/Assets/Variables.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variables : MonoBehaviour 
+{
+	string varTypes = "There are many different types of variables. This is a string variable.";
+	string ex02 = "Next is an example of an int.";
+	int example = 52;
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log (varTypes);
+		print (ex02);
+		print (example);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
