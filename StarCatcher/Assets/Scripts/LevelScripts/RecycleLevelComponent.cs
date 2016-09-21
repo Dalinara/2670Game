36de71b228de99777743156d58143f903cd7af44
Unknown <dalinara13@gmail.com>
2016-09-21 14:01:53 -0700

    Recycle Level update

--- StarCatcher/Assets/Scripts/LevelScripts/RecycleLevelComponent.cs
+++ StarCatcher/Assets/Scripts/LevelScripts/RecycleLevelComponent.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RecycleLevelComponent : MonoBehaviour 
 {
 	private Vector3 newLocation;
+	public List<Recycler> recyclableList;
+	private int i = 0;
+
+	void Start()
+	{
+		recyclableList = new List<Recycler> ();
+		Recycler.recycleAction += RecycleActionHandler;
+	}
+
+	private void RecycleActionHandler(Recycler _r)
+	{
+		recyclableList.Add (_r);
+	}
 
 	void OnTriggerEnter()
 	{
-//		print ("Hit!");
 		newLocation.x = StaticVars.nextSectionPos;
-		transform.position = newLocation;
+		recyclableList[i].cube.position = newLocation;
 		StaticVars.nextSectionPos += StaticVars.distance;
-
+		if (i < recyclableList.Count - 1) 
+		{
+			i++;
+		}
 	}
 }
---
+++ StarCatcher/Assets/Scripts/LevelScripts/Recycler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Recycler : MonoBehaviour 
+{
+	//Create a delegate
+	public static Action<Recycler> recycleAction;
+	//
+	public bool canBeRecycled = false;
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cube = this.GetComponent<Transform> ();
+		//Test to see if the delegate is null, and if not run the delegate 
+		if (recycleAction != null && canBeRecycled != false) 
+		{
+			//Send a data type of recycleAction
+			recycleAction (this);
+		}
+	}
+
+	void OnTriggerEnter ()
+	{
+		canBeRecycled = true;
+		Start ();
+	}
+
+}
