29428ff9811a857fed8bed8179144a245bee9485
Unknown <dalinara13@gmail.com>
2016-09-20 23:56:59 -0700

    Loops Assignment

---
+++ ScriptingAssignments/Assets/Scripts/IfAndSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfAndSwitch : MonoBehaviour
+{
+	public static int life = 3;
+	//Vector 3 to store the position
+	Vector3 spawnPoint;
+	// Use this for initialization
+	void Start () 
+	{
+		//GameObject that will find the object tagged "Respawn" in the game
+		GameObject spawnObject = GameObject.FindGameObjectWithTag ("Respawn");
+		//set the spawnPoint to the object
+		spawnPoint = spawnObject.transform.position;
+	}
+	//OnTriggerEnter method that will run when the player runs into an object tagged "Wall"
+	void OnTriggerEnter(Collider other)
+	{
+		//Respawn if the player hits a wall
+		if (other.gameObject.tag == "Wall") 
+		{
+			//Moves the player back to the Respawn position
+			transform.position = spawnPoint;
+			life--;
+		}
+	}
+}
---
+++ ScriptingAssignments/Assets/Scripts/Loops.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour 
+{
+	Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		//GameObject that will find the object tagged "Respawn" in the game
+		GameObject spawnObject = GameObject.FindGameObjectWithTag ("Respawn");
+		//set the spawnPoint to the object
+		spawnPoint = spawnObject.transform.position;
+
+	}
+	//
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Do While loop to tell the player how many lives they have
+		do 
+		{
+			//Tell the player how many lives they have left
+			print ("You have " + IfAndSwitch.life + " lives left");
+		} while(IfAndSwitch.life >= 0);
+	}
+}
