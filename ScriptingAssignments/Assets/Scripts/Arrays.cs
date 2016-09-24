0cd3b400e1ddcb79194e445638ff1cd42b3bcde6
Unknown <dalinara13@gmail.com>
2016-09-23 23:38:38 -0700

    Arrays and Lists assignment

---
+++ ScriptingAssignments/Assets/Scripts/Arrays.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour 
+{
+	//string array of possible weapons
+	public string [] weapons = {"sword", "gun", "dagger", "bow", "staff"};
+	//int to count the number in the array
+	int i = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("Press 'Z' to switch your weapon.");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Z) && i < weapons.Length) 
+		{
+			print ("You have equiped your " + weapons[i]);
+			i++;
+		}
+		if (i == weapons.Length) 
+		{
+			i = 0;
+		}
+	}
+}
