e83c14572234da34afda818e7723130e7f41b7c8
Unknown <dalinara13@gmail.com>
2016-10-24 13:02:12 -0700

    Update

---
+++ StarCatcher/Assets/Scripts/Character/MoveUsingArrowKeys.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUsingArrowKeys : MonoBehaviour 
+{
+	private CharacterController cc;
+	public Vector3 moveDirection;
+	int jumpSpeed = 10;
+	int gravity = 1;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void Rotate(int _rotateInY)
+	{
+		Transform.Rotate (0, _rotateInY, 0);
+	}
+
+	void Move(float _moveInZ)
+	{
+		if (cc.isGrounded) 
+		{
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				print ("jump");
+				moveDirection.y += jumpSpeed * Time.deltaTime;
+			}
+				
+			moveDirection.y -= gravity * Time.deltaTime;			
+
+			moveDirection.x = _moveInZ;
+			moveDirection - transform.TransformDirection (moveDirection * speed * Time.deltaTime);
+			cc.Move (moveDirection);
+		}
+
+	}
+
+	public void MoveOnArrows()
+	{
+
+	}
+
+}
