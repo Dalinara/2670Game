66cba8250eda105d5faffb56f3c288dd2c4356b9
Unknown <dalinara13@gmail.com>
2016-11-04 13:56:16 -0700

    Updated the character move

---
+++ StarCatcher/Assets/Scripts/Character/CharMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour 
+{
+	private CharacterController cc;
+	public Vector3 moveDirection;
+	public float jumpSpeed = 10;
+	public float gravity = 1;
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cc = GetComponent<CharacterController> ();
+		MoveUsingArrowKeys.MoveOnArrows += Move;
+		MoveUsingArrowKeys.RotateOnArrows += Rotate;
+	}
+
+	void Rotate(int _rotateInY)
+	{
+		//Transform.Rotate (0, _rotateInY, 0);
+
+	}
+
+	void Move(float _moveInZ)
+	{
+		if (cc.isGrounded) 
+		{
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				print ("jump");
+				moveDirection.y += jumpSpeed * Time.deltaTime;
+			}
+
+			moveDirection.y -= gravity * Time.deltaTime;
+
+			moveDirection.x = _moveInZ;
+			//moveDirection - transform.TransformDirection (moveDirection * speed * Time.deltaTime);
+			moveDirection = transform.TransformDirection(moveDirection * Time.deltaTime);
+			cc.Move (moveDirection);
+		}
+	}
+}
--- StarCatcher/Assets/Scripts/Character/MoveUsingArrowKeys.cs
+++ StarCatcher/Assets/Scripts/Character/MoveUsingArrowKeys.cs
@@ -1,53 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MoveUsingArrowKeys : MonoBehaviour 
 {
-	private CharacterController cc;
-	public Vector3 moveDirection;
-	int jumpSpeed = 10;
-	int gravity = 1;
-	int speed = 1;
+	public static Action<float> MoveOnArrows;
+	public static Action<float> RotateOnArrows;
 
-	// Use this for initialization
-	void Start () 
+	void Update()
 	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
-	}
-
-	void Rotate(int _rotateInY)
-	{
-//		Transform.Rotate (0, _rotateInY, 0);
-	}
-
-	void Move(float _moveInZ)
-	{
-		if (cc.isGrounded) 
+		if (MoveOnArrows != null) 
 		{
-			if (Input.GetKey (KeyCode.Space)) 
-			{
-				print ("jump");
-				moveDirection.y += jumpSpeed * Time.deltaTime;
-			}
-				
-			moveDirection.y -= gravity * Time.deltaTime;			
-
-			moveDirection.x = _moveInZ;
-//			moveDirection - transform.TransformDirection (moveDirection * speed * Time.deltaTime);
-			cc.Move (moveDirection);
+			MoveOnArrows (Input.GetAxis ("Vertical"));
 		}
 
+		if (RotateOnArrows != null) 
+		{
+			RotateOnArrows (Input.GetAxis ("Horizontal"));
+		}
 	}
-
-	public void MoveOnArrows()
-	{
-
-	}
-
 }
