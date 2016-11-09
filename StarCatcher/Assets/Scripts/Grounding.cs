ef3c356bb7cd4963dfc71e3835cd52a10ae69d05
Unknown <dalinara13@gmail.com>
2016-11-09 13:56:07 -0700

    IsGrounded

---
+++ StarCatcher/Assets/Scripts/Grounding.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Grounding : MonoBehaviour 
+{
+	private CharacterController cc;
+	private float gravity = -5;
+	private Vector3 tempP;
+	public Vector3 startPoint;
+	public float speed = 20;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startPoint = transform.position;
+		cc = GetComponent<CharacterController> ();
+	}
+
+	void OnTriggerEnter()
+	{
+		speed *= -1;
+//		if (cc.velocity.x < 40) 
+//		{
+//			gravity *= 1.5f;
+//		}
+//		transform.position = startPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		print (cc.velocity);
+		tempP.y = gravity;
+		cc.Move(tempP * Time.deltaTime);
+		if (cc.isGrounded) 
+		{
+			tempP.x = speed;
+		} else 
+		{
+			tempP.x = 0;
+		}
+	}
+}
