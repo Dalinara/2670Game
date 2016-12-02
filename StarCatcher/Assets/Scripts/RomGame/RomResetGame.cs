307511e9eabe7139e07aaade0f75465a79749673
Unknown <dalinara13@gmail.com>
2016-12-02 13:45:53 -0700

    Reset practice

---
+++ StarCatcher/Assets/Scripts/RomGame/RomResetGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RomResetGame : MonoBehaviour 
+{
+	public static Action ResetLevel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		EndGame.GameOver += ResetLevel;
+	}
+
+
+
+}
