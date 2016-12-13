026e4b06dc9a8a9ba3e33472559ad4f25a132eb3
Unknown <dalinara13@gmail.com>
2016-12-13 11:54:32 -0700

    Update

---
+++ Final/Assets/Scripts/LightPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPowerUp : MonoBehaviour , IPowerUp
+{
+	public GameObject torchLight;
+
+	void Start()
+	{
+		torchLight.SetActive (false);
+	}
+
+	void OntriggerEnter()
+	{
+		torchLight.SetActive (true);
+	}
+
+}
---
+++ Final/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour 
+{
+	private CharacterController cc;
+	public Vector3 moveDirection;
+	public float speed = 10;
+	public float jumpSpeed = 10;
+	public float gravity = 1;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cc = GetComponent <CharacterController> ();
+	}
+
+
+}
