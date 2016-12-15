d01a7d42dc79c6284cb7cea1a6718afccd057010
Unknown <dalinara13@gmail.com>
2016-12-15 00:16:41 -0700

    Update

---
+++ Final/Assets/Scripts/Death.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour 
+{
+//	enum DeathType {Poison, Shot, Stabbed, OldAge};
+//
+//	// Use this for initialization
+//	void Start () 
+//	{
+//		StartCoroutine (CharacterDeath());
+//	}
+//
+//	IEnumerator CharacterDeath()
+//	{
+//		DeathType DT = null;
+//		switch (DeathType) 
+//		{
+//		case DeathType.Poison:
+//			print ("You have died by poison");
+//			DT = DeathType.Poison;
+//			break;
+//		case DeathType.Shot:
+//			print ("You have been shot.");
+//			DT = DeathType.Shot;
+//			break;
+//		case DeathType.Stabbed:
+//			print ("You have been stabbed.");
+//			DT = DeathType.Stabbed;
+//			break;
+//		case DeathType.OldAge:
+//			print ("Congratulations! You have lived a long life and have died of old age.");
+//			DT = DeathType.OldAge;
+//			break;
+//		default:
+//			print ("Unable to die. You are immortal.");
+//			DT = null;
+//			break;
+//		}
+//		return DT;
+//	}
+
+}
--- Final/Assets/Scripts/Display/WinGame.cs
+++ Final/Assets/Scripts/Display/WinGame.cs
@@ -4,7 +4,17 @@ using UnityEngine.UI;
 
 public class WinGame : MonoBehaviour 
 {
+	public GameObject winner;
 
+	void Start()
+	{
+		winner.SetActive (false);
+	}
 
+	void OnTriggerEnter()
+	{
+		print ("You win!");
+		winner.SetActive (true);
+	}
 
 }
